71e982e966cf69affaeac440502268b42c4a5095
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-09 11:07:11 +0000

    Initial Commit

---
+++ ESSI/ESSIApp.swift
@@ -0,0 +1,18 @@
+//
+//  ESSIApp.swift
+//  ESSI
+//
+//  Created by app-kaihatsusha on 09/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ESSIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
